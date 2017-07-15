ecf7679c6466b0e8efe74129d9dbf97d024c8b9c
valler <valery.kaptanouski@instinctools.ru>
2017-07-16 00:21:33 +0300

    Changing class name

--- sandbox/src/main/java/by/stqa/pft/sandbox/DooBee.java
+++ sandbox/src/main/java/by/stqa/pft/sandbox/DooBee.java
@@ -1,14 +1,14 @@
 package by.stqa.pft.sandbox;
 
-public class DooBeeNew2 {
-    public static void main (String[] args){
+public class DooBee {
+    public static void main(String[] args) {
         int x = 1;
-        while (x < 3){
+        while (x < 3) {
             System.out.print("Doo");
             System.out.print("Bee");
             x = x + 1;
         }
-        if (x == 3){
+        if (x == 3) {
             System.out.print("Do");
         }
     }
