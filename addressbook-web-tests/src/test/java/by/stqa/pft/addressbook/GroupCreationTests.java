a69cd02af27f79d7ebb3a77a70792d8a217fbfcc
valler <valery.kaptanouski@instinctools.ru>
2017-07-18 22:41:44 +0300

    Get application start page and login have been method from test method to fixture initialization method.

--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/GroupCreationTests.java
@@ -23,10 +23,6 @@ public class GroupCreationTests {
 
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupCreationTests() {
         wd.get("http://localhost/addressbook/index.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -35,6 +31,10 @@ public class GroupCreationTests {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+    
+    @Test
+    public void testGroupCreation () {
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
